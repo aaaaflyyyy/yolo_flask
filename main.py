99dd6d47d10defe1e238c823544c18e01504daaa
aaaaflyyyy <69952459+aaaaflyyyy@users.noreply.github.com>
2021-05-09 17:43:42 +0800

    first commit.

---
+++ main.py
@@ -0,0 +1,169 @@
+# Import necessary libraries
+from flask import *
+
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+
+
+# Model Path
+PATH = './yolov3.pt'
+num = [0]
+
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+
+def detect():
+    source = '0'
+    weights = 'yolov3.pt'
+    imgsz = 640
+
+    view_img = False
+    save_txt = False
+    save_img = False
+
+    webcam = True if source == '0' else False
+
+    # Directories
+    save_dir = Path(increment_path(
+        Path('runs/detect') / 'exp', exist_ok=False))
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)
+
+    # Initialize
+    set_logging()
+    device = select_device('')
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    if half:
+        model.half()  # to FP16
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(
+            next(model.parameters())))  # run once
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = time_synchronized()
+        pred = model(img, augment=False)[0]
+
+        # Apply NMS
+        conf_thres = 0.25
+        iou_thres = 0.45
+        pred = non_max_suppression(
+            pred, conf_thres, iou_thres, classes=None, agnostic=False)
+        t2 = time_synchronized()
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(
+                ), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + \
+                ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            s += '%gx%g ' % img.shape[2:]  # print string
+            # normalization gain whwh
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(
+                    img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+
+                    if int(c) != 0:
+                        continue
+
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    num[0] = int(n)
+
+                    # add to string
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
+
+                # Write results
+                save_conf = False
+
+                for *xyxy, conf, cls in reversed(det):
+
+                    if int(cls) != 0:
+                        continue
+
+                    if save_img or view_img:  # Add bbox to image
+                        label = f'{names[int(cls)]} {conf:.2f}'
+                        plot_one_box(xyxy, im0, label=label,
+                                     color=colors[int(cls)], line_thickness=3)
+
+            # # Print time (inference + NMS)
+            # print(f'{s}Done. ({t2 - t1:.3f}s)')
+
+            # Stream results
+            if view_img:
+                ret, buffer = cv2.imencode('.jpg', im0)
+                im0 = buffer.tobytes()
+                yield(b'--frame\r\n'
+                      b'Content-Type: image/jpeg\r\n\r\n' + im0 + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/updata_data', methods=['GET'])
+def update_data():
+    global num
+    return jsonify(num=num)
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(detect(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
